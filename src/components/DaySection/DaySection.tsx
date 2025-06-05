7c3c5b1b2fdf5e9f35f6adb8caf7d6bc41818e42
rom <filpoyma@gmail.com>
2025-06-05 21:19:22 +0300

    day

---
+++ src/components/DaySection/DaySection.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './DaySection.module.css';
+
+const DaySection: React.FC = () => {
+  return (
+    <div className={styles.container} id="day">
+      <h2>День 1</h2>
+      {/* Здесь будет контент */}
+    </div>
+  );
+};
+
+export default DaySection; 

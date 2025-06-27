ddfd42084c471d34d5da65e6fac0cba308502f2d
rom <filpoyma@gmail.com>
2025-06-27 12:00:17 +0300

    dayN

--- src/pages/AdminPanel/components/SectionHeader.tsx
+++ src/pages/AdminPanel/components/SectionHeader.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Plus } from 'lucide-react';
+import Plus from '../../../assets/icons/plus.svg';
 import styles from '../AdminPanel.module.css';
 
 interface SectionHeaderProps {
@@ -8,35 +8,30 @@ interface SectionHeaderProps {
   isCreating?: boolean;
 }
 
-function getButtonText(title: string) {
-  switch (true) {
-    case /путешествий/i.test(title):
-      return 'Создать программу';
-    case /ресторан/i.test(title):
-      return 'Создать ресторан';
-    case /отелей/i.test(title):
-      return 'Создать отель';
-    default:
-      return 'Создать';
-  }
-}
+// function getButtonText(title: string) {
+//   switch (true) {
+//     case /путешествий/i.test(title):
+//       return 'Создать программу';
+//     case /ресторан/i.test(title):
+//       return 'Создать ресторан';
+//     case /отелей/i.test(title):
+//       return 'Создать отель';
+//     default:
+//       return 'Создать';
+//   }
+// }
 
-export const SectionHeader: FC<SectionHeaderProps> = ({ 
-  title, 
-  onCreateClick, 
-  isCreating = false 
+export const SectionHeader: FC<SectionHeaderProps> = ({
+  title,
+  onCreateClick,
+  isCreating = false,
 }) => {
   return (
     <div className={styles.header}>
       <h2>{title}</h2>
-      <button
-        className={styles.createButton}
-        onClick={onCreateClick}
-        disabled={isCreating}
-      >
-        <Plus size={20} />
-        {getButtonText(title)}
+      <button className={styles.createButton} onClick={onCreateClick} disabled={isCreating}>
+        <Plus height={20} width={20} />
       </button>
     </div>
   );
-}; 
+};
